0a859cdedbb5711b32dfecc68048f110cc72d6cd
Almichot-1 <Almichot-1@users.noreply.github.com>
2026-04-02 19:39:28 +0300

    Enable RLS on sensitive tables

---
+++ migrations/000020_enable_rls_sensitive_tables.down.sql
@@ -0,0 +1,6 @@
+alter table public.passport_data disable row level security;
+alter table public.medical_data disable row level security;
+alter table public.password_reset_requests disable row level security;
+alter table public.admin_setup_tokens disable row level security;
+alter table public.user_sessions disable row level security;
+
